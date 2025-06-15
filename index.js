ebe738e07e945a0887aa04623dfb5268e08d6903
Unwana Udosen <unwanaudosen8@gmail.com>
2025-06-15 14:18:48 +0100

    Added form handler and typewriter plugin

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-
+function poemGenerator(event){
+          event.preventDefault();
+          
+          new Typewriter('#poem', {
+  strings:"esta agua que fluye de la noche",
+  autoStart: true,
+  delay: 1,
+  cursor: '',
+});
+      }
+      
+      let poemElement = document.querySelector("#form");
+      poemElement.addEventListener("submit", poemGenerator);
