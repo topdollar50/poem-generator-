dec390abe458c0643c181e707ca9af74cec3dd21
Unwana Udosen <unwanaudosen8@gmail.com>
2025-06-16 19:49:54 +0100

    Added the function to receive the value of users input in the form

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-function poemGenerator(event){
-          event.preventDefault();
-          
+function displayPoem (response){
+      let poem = document.querySelector("#poem");
+      poem.innerHTML = response.data.answer
           new Typewriter('#poem', {
-  strings:"esta agua que fluye de la noche",
+  strings:response.data.answer,
   autoStart: true,
   delay: 1,
   cursor: '',
 });
+      }
+      function poemGenerator(event){
+          event.preventDefault();
+          let apiKey = `fa0d396f3af7cta73o6fb5a74235503f`
+          let poemInput = document.querySelector("#poem-input");
+          let prompt = poemInput.value;
+      let context = `write a spanish poem of five lines about the  ${prompt}  in HTML format and sign it at the end with <strong style = "color:green">SheCodes AI</strong> without including backtick and html`
+      
+      let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`
+      axios.get(apiUrl).then(displayPoem);
       }
       
       let poemElement = document.querySelector("#form");
